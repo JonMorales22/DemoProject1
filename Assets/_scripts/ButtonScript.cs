f8a86b35ada02675aa32bc23b2f95a6a38df0799
Jonathan Morales <jonmorales22@gmail.com>
2016-09-24 18:56:42 -0400

    initial commit

---
+++ Assets/_scripts/ButtonScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class ButtonScript : MonoBehaviour {
+
+	/// <summary>
+	/// Performs some of the button functionalities. 
+	/// </summary>
+	public void GameScreen() {
+		SceneManager.LoadScene ("GameScreen");
+	}
+
+	public void HighScoresScreen() {
+		SceneManager.LoadScene ("HighScores");
+	}
+
+	public void ClearScores(){
+		PlayerPrefs.DeleteAll ();
+	}
+
+	public void ExitGame() {
+		Application.Quit;
+	}
+}
