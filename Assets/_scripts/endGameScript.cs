f1256d04da1208296a74ab10cd6724687200bd58
Jonathan Morales <jonmorales22@gmail.com>
2016-09-24 21:33:06 -0400

    The REAL first commit

---
+++ Assets/_scripts/endGameScript.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class endGameScript : MonoBehaviour {
+	public GameObject cube;
+
+	public void endGame()
+	{
+		int score = cube.GetComponent<CubeScript> ().score;
+		PlayerPrefs.SetInt ("247127CurrentPlayer",score);
+		SceneManager.LoadScene ("HighScores");
+	}
+}
---
+++ Assets/_scripts/welcomeScript.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class welcomeScript : MonoBehaviour {
+
+	/// <summary>
+	/// Searches player prefs and deletes any "CurrentPlayer." If this isn't deleted, issues occur later when the high scores are displayed.
+	///  Wanted to find a better solution this issue, but whatever....
+	/// </summary>
+	void Start () {
+		PlayerPrefs.DeleteKey ("247127CurrentPlayer");	
+	}
+
+}
